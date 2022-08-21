df3f43e9c660ce7e286849d8f3bea17144119ab1
narendrasinghrathore <rathoreofficialemail@gmail.com>
2022-08-21 11:00:47 +0530

    Initial Setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'posts',
+        loadChildren: () =>
+          import('../components/post/post.module').then((m) => m.PostModule),
+      },
+      {
+        path: '',
+        loadChildren: () =>
+          import('../components/home/home.module').then((m) => m.HomeModule),
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
